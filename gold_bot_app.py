1456ad309702c648b90d8bc215f58f9c8b8bc6d3
TB-afk <tariktroq@gmail.com>
2025-06-29 20:17:00 +0200

    Update gold_bot_app.py

--- gold_bot_app.py
+++ gold_bot_app.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import pandas as pd
 import streamlit as st
-from MAC import ta.momentum 
+from MACD import ta.momentum 
 import RSIIndicator
 import plotly.graph_objects as go
 
