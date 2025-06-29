276d88afc1901df898caf7052aaafe471451553d
TB-afk <tariktroq@gmail.com>
2025-06-29 15:21:17 +0200

    Update gold_bot_app.py

--- gold_bot_app.py
+++ gold_bot_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-from sklearn.ensemble import 
+from sklearn.ensemble 
+import 
 RandomForestClassifier
 
 st.set_page_config(page_title="Gold Swing Bot", page_icon="📈")
